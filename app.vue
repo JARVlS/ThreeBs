4407a2348fd3f839fb1b4aa9fbe093cbcc4f664b
Leon <leon.grass@gmx.net>
2023-08-29 20:48:45 +0200

    setup and landing page

--- app.vue
+++ app.vue
@@ -1,5 +1,55 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="page">
+    <Header></Header>
+    <NuxtPage />
   </div>
 </template>
+
+<style>
+@font-face {
+  font-family: Brolink;
+  src: url("assets/fonts/brolink/BrolinkDemo-VGyMZ.otf");
+}
+@font-face {
+  font-family: Nunito;
+  src: url('assets/fonts/Nunito/Nunito-VariableFont_wght.ttf');
+}
+:root {
+  --color-bg: #000;
+  --color-text: #fff;
+  --color-shadows: #555555;
+}
+
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Nunito, Calibri, sans-serif;
+}
+
+.page{
+  background-color: var(--color-bg);
+  color: var(--color-text);
+  height: 100vh;
+  width: 100vw;
+  overflow:auto;
+  position: relative;
+  padding-top: 13rem;
+}
+
+.headline{
+  font-family: Nunito;
+}
+
+.text_big{
+  font-size: 1.2rem;
+  font-weight: 600;
+}
+.header{
+  position:fixed;
+  top:0; 
+  width: 100vw;
+  height: auto;
+  background-color: var(--color-bg);
+}
+</style>
